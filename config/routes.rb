c2264d7f1a954eaf36e7cf74c22bb8d7696236cb
yani82 <yani.iman1@gmail.com>
2021-01-10 01:20:48 -0500

    commit tree c580ec9f288adb6ab93b8e7fed5bc6a32f29fcac

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get '/welcome' => 'users#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create' 
+  post '/logout' => 'sessions#destroy' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
